0ed5859bc652e96beb50e8d8d02ec39a395dac5c
jed peek <jedpeek@MacBook-Pro-3.local>
2024-03-17 16:22:46 -0700

    updated footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,4 +1,5 @@
-import { Instagram, Linkedin } from "lucide-react";
+import { Instagram, Linkedin, LinkedinIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
@@ -6,12 +7,14 @@ const Footer = () => {
     <div className="mt-5 p-5 border">
       <div className="flex justify-between">
         <div>
-          <h1>@jedpeek</h1>
-          <h1>@jedpeek</h1>
+          <Link href="https://www.linkedin.com/in/jed-peek/">
+            <h1>@jedpeek 2024</h1>
+          </Link>
         </div>
         <div>
-          <Instagram />
-          <Linkedin />
+          <Link href="https://www.linkedin.com/in/jed-peek/">
+            <LinkedinIcon />
+          </Link>
         </div>
       </div>
     </div>
