0551d334bec79534703878e3a90e7f2bbe4ec4f0
jed peek <jedpeek@MacBook-Pro-3.local>
2024-03-17 15:54:25 -0700

    updated provider url

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -11,8 +11,8 @@ export default function CartProvider({
       currency="USD"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://music-store-henna.vercel.app/stripe/success"
+      cancelUrl="https://music-store-henna.vercel.app/stripe/error"
       billingAddressCollection={false}
       shouldPersist={true}
       language="en-US"
